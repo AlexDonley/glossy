db6b61005194d039cd3c6a046613cc9ec8047455
AlexDonley <iamdonley98@gmail.com>
2024-10-19 23:35:12 +0800

    draggable glosses

--- script.js
+++ script.js
@@ -114,33 +114,54 @@ function addGloss(n) {
 
     glossEntries.prepend(newEntry)
 
-    //updateDraggables()
+    updateDraggables()
 }
 
-// function updateDraggables() {
-//     draggables = document.querySelectorAll('.draggable')
+function updateDraggables() {
+    draggables = document.querySelectorAll('.draggable')
 
-//     draggables.forEach(item => {
-//         item.addEventListener('dragstart', () => {
-//             console.log('drag start')
-//             item.classList.add('dragging')
-//         })
+    draggables.forEach(item => {
+        item.addEventListener('dragstart', () => {
+            console.log('drag start')
+            item.classList.add('dragging')
+        })
 
-//         item.addEventListener('dragend', () => {
-//             item.classList.remove('dragging')
-//         })
-//     })
-// }
+        item.addEventListener('dragend', () => {
+            console.log('drag end')
+            item.classList.remove('dragging')
+        })
+    })
+}
 
-// glossEntries.addEventListener('dragover', e => {
+glossEntries.addEventListener('dragover', e => {
+    // console.log('dragging over')
+    e.preventDefault()
+    const afterElement = placeDraggedElement(e.clientY)
+    console.log(afterElement)
 
-// })
+    currentDrag = document.querySelector('.dragging')
 
-// function placeDraggedElement(y) {
-//     const draggableElements = [...document.querySelectorAll('draggable:not(.dragging)')]
+    if (afterElement == null) {
+        glossEntries.appendChild(currentDrag)
+    } else {
+        glossEntries.insertBefore(currentDrag, afterElement)
+    }
+})
 
-//     draggableElements.reduce()
-// }
+function placeDraggedElement(y) {
+    const draggableElements = [...document.querySelectorAll('.draggable:not(.dragging)')]
+
+    return draggableElements.reduce((closest, child) => {
+        const box = child.getBoundingClientRect()
+        const offset = y - box.top - box.height/2
+            if (offset < 0 && offset > closest.offset){
+                return { offset: offset, element: child}
+            } else {
+                return closest
+            }
+
+    }, {offset: Number.NEGATIVE_INFINITY}).element
+}
 
 function changeTextSize(n, str) {
     
