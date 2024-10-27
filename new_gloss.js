1ccf1b95b0f0ec887ec6247aa68a34c6787ab0be
AlexDonley <iamdonley98@gmail.com>
2024-10-27 18:36:41 +0800

    additional editing function

--- new_gloss.js
+++ new_gloss.js
@@ -1,18 +1,40 @@
 const newLineTemplate = document.querySelector("[line-entry-template]")
 const gridColumn = document.getElementById('gridColumn')
 
-lineCount = 0
-
-console.log(newLineTemplate)
+var lineCount = 0
+var totalLines = 0
 
 function addNewLine() {
     const newLine = newLineTemplate.content.cloneNode(true)
     for (var i = 0; i < newLine.children.length; i++) {
         var child = newLine.children[i]
         child.classList.add('line' + lineCount)
+
+        if (i == 1) {
+            
+            command = "deleteLine("+ lineCount +")"
+            
+            child.setAttribute('onclick', command)
+        }
     }
+    
     gridColumn.append(newLine)
     lineCount++
+    totalLines++
+}
+
+function deleteLine(n) {
+    
+    if (totalLines > 1) {
+        lineElements = document.getElementsByClassName('line' + n);
+        console.log(lineElements)
+    
+        while(lineElements[0]) {
+            lineElements[0].parentNode.removeChild(lineElements[0]);
+        }
+
+        totalLines--
+    }
 }
 
 addNewLine()
