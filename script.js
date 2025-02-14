3d5aef5c9d9832055eac2b0cf299b2368be91d85
Alex Donley <152264753+AlexDonley@users.noreply.github.com>
2025-02-14 11:41:53 +0800

    Update script.js

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ var text_size = 20
 var gloss_size = 40
 var menuTog = true
 
-const availableGlosses = ['p3', 'p6', 'p7', 'p8', 'p9', 'p10', 'p11', 'halloween', 'christmas']
+const availableGlosses = ['p3', 'p6', 'p7', 'p8', 'p9', 'p10', 'p11', 'p12', 'halloween', 'christmas']
 const sessionKeys = Object.keys(sessionStorage)
 
 let translated = []
