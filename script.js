09ff79987c4996710759fc2b4e2a666b858f4201
AlexDonley <iamdonley98@gmail.com>
2025-03-01 11:17:43 +0800

    automatically resizing text

--- script.js
+++ script.js
@@ -128,9 +128,8 @@ window.addEventListener('mousedown', (e) => {
         const targetIndex = e.target.id.substring(1)
         
         const targetWord = currentSentences[targetIndex].english
-        
-        highLight.innerText = targetWord
-        highLight.style.fontSize = 1000 / (targetWord.length + 4) + "pt"
+
+        fillMaxFontWidth(highLight, targetWord, 15);
 
         let translation_check = targetWord.toLowerCase() + "_" + currentSentences[targetIndex].chinese
         console.log(translation_check)
@@ -347,3 +346,58 @@ function togglePinyin(idx) {
         }
     }
 }
+
+function fillMaxFontWidth(elem, word, margin, min) {
+    
+    const parentLimits = elem.getBoundingClientRect();
+
+    let changeFS = 1;
+
+    if (min) {
+        changeFS = min;
+    }
+
+    const childDiv = document.createElement('div');
+    childDiv.innerText = word;
+    childDiv.style.margin = 'auto';
+    childDiv.style.fontSize = changeFS + "pt";
+
+    elem.innerHTML = '';
+    elem.append(childDiv);
+
+    // linear method 
+    // only creates one line of text
+    // TODO: figure out how to optimize line breaks
+
+    while (
+        childDiv.getBoundingClientRect().width < parentLimits.width - (margin * 2)
+        && childDiv.getBoundingClientRect().height < parentLimits.height
+    ) {
+        changeFS++
+        childDiv.style.fontSize = changeFS + "pt"
+    }
+
+    changeFS--
+    childDiv.style.fontSize = changeFS + "pt"
+
+    const newDimensions = childDiv.getBoundingClientRect()
+    console.log(parentLimits, newDimensions, changeFS)
+}
+
+window.addEventListener('resize', (event) => {
+    resizeLimitedTexts()
+})
+
+function resizeLimitedTexts() {
+    const allResize = document.querySelectorAll('.resize-txt');
+
+    allResize.forEach(elem => {
+        if (elem.children[0]) {
+            const inTxt = elem.children[0].innerText;
+            console.log(inTxt)
+            fillMaxFontWidth(elem, inTxt, 15);
+        }
+    })
+}
+
+resizeLimitedTexts()
