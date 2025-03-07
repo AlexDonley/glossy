194921f23c98ca3428a373e312aa4874d05bd4c4
Alex Donley <152264753+AlexDonley@users.noreply.github.com>
2025-03-07 13:43:54 +0800

    Update script.js

--- script.js
+++ script.js
@@ -176,7 +176,7 @@ function addGloss(n) {
     const newEntry = document.createElement('div')
     newEntry.classList = 'entry new draggable'
     newEntry.id = 'gloss' + totalGlosses
-    newEntry.setAttribute("draggable", true)
+    //newEntry.setAttribute("draggable", true)
     
     const engSpan = document.createElement('span')
     engSpan.innerText = currentSentences[n].english
@@ -213,42 +213,47 @@ function addGloss(n) {
     newCheck.classList.add('py-check');
     newCheck.addEventListener('change', togglePinyin(totalGlosses));
     
-    const prevNew = document.getElementsByClassName('new')
+    const prevNew = document.getElementsByClassName('new');
     if (prevNew[0]){
-        prevNew[0].classList.remove('new')
+        prevNew[0].classList.remove('new');
     }
 
     newEntry.append(newCheck);
 
+    const dragHandle = document.createElement('div');
+    dragHandle.classList.add('drag-handle');
+    dragHandle.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" height="36px" viewBox="0 -960 960 960" width="36px" fill="#333333"><path d="M360-160q-33 0-56.5-23.5T280-240q0-33 23.5-56.5T360-320q33 0 56.5 23.5T440-240q0 33-23.5 56.5T360-160Zm240 0q-33 0-56.5-23.5T520-240q0-33 23.5-56.5T600-320q33 0 56.5 23.5T680-240q0 33-23.5 56.5T600-160ZM360-400q-33 0-56.5-23.5T280-480q0-33 23.5-56.5T360-560q33 0 56.5 23.5T440-480q0 33-23.5 56.5T360-400Zm240 0q-33 0-56.5-23.5T520-480q0-33 23.5-56.5T600-560q33 0 56.5 23.5T680-480q0 33-23.5 56.5T600-400ZM360-640q-33 0-56.5-23.5T280-720q0-33 23.5-56.5T360-800q33 0 56.5 23.5T440-720q0 33-23.5 56.5T360-640Zm240 0q-33 0-56.5-23.5T520-720q0-33 23.5-56.5T600-800q33 0 56.5 23.5T680-720q0 33-23.5 56.5T600-640Z"/></svg>'
+
+    dragHandle.addEventListener('mousedown' || 'touchstart', () => {
+        console.log('mouse down');
+        newEntry.setAttribute('draggable', true);
+        newEntry.classList.add('dragging');
+    })
+
+    dragHandle.addEventListener('mouseup' || 'touchend', () => {
+        console.log('mouse up');
+        newEntry.setAttribute('draggable', false);
+        newEntry.classList.remove('dragging');
+    })
+
+    newEntry.addEventListener('dragend', () => {
+        console.log('drag end');
+        newEntry.setAttribute('draggable', false);
+        newEntry.classList.remove('dragging');
+    })
+
+    newEntry.append(dragHandle)
+
     glossEntries.prepend(newEntry);
     totalGlosses++
 
-
-
-    updateDraggables()
-}
-
-function updateDraggables() {
-    const draggables = document.querySelectorAll('.draggable')
-
-    draggables.forEach(item => {
-        item.addEventListener('dragstart', () => {
-            console.log('drag start')
-            item.classList.add('dragging')
-        })
-
-        item.addEventListener('dragend', () => {
-            console.log('drag end')
-            item.classList.remove('dragging')
-        })
-    })
+    // updateDragHandles()
 }
 
 glossEntries.addEventListener('dragover', e => {
     
     e.preventDefault()
     const afterElement = placeDraggedElement(e.clientY)
-    console.log(afterElement)
 
     const currentDrag = document.querySelector('.dragging')
 
@@ -261,6 +266,7 @@ glossEntries.addEventListener('dragover', e => {
 
 function placeDraggedElement(y) {
     const draggableElements = [...document.querySelectorAll('.draggable:not(.dragging)')]
+
 
     return draggableElements.reduce((closest, child) => {
         const box = child.getBoundingClientRect()
