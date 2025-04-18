318c41af3c29b15d572a0ccf70b5fb8e32fd6fcf
Alex Donley <152264753+AlexDonley@users.noreply.github.com>
2025-04-18 09:20:49 +0800

    Update script.js

--- script.js
+++ script.js
@@ -40,7 +40,7 @@ let menuTog = true;
 const availableGlosses = [
     'p3', 'p6', 'p7', 'p8', 'p9', 
     'p10', 'p11', 'p12', 'p13', 'p14', 
-    'p15', 'p16', 'p17',
+    'p15', 'p16', 'p17', 'p18', 'p19',
     'halloween', 'christmas', 'children'
 ]
 const sessionKeys = Object.keys(sessionStorage)
